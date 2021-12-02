8e6d51547c8c309d4d2909a5fcac39691ef0f60d
Wilworin <Wilworin@users.noreply.github.com>
2021-12-02 23:38:19 +0100

    Add project files.

---
+++ Advent of Code 2021/Day1/Day1.cs
@@ -0,0 +1,48 @@
+﻿// Advent of Code. Day 1, Assignment 1.
+
+class Day1
+{
+    public static void Challenge1()
+    {
+        Helper helper = new Helper();
+
+        string fileName = "Day1\\input.txt";
+        List<int> input = helper.ReadFileIntoIntList(fileName);
+
+        int lastNumber = 999999999;
+        int counter = 0;
+        foreach (int number in input)
+        {
+            if (number > lastNumber)
+            {
+                counter++;
+            }
+            lastNumber = number;
+        }
+        Console.WriteLine("Day1. Challenge 1: " + counter);
+    }
+
+    public static void Challenge2()
+    {
+        Helper helper = new Helper();
+
+        string fileName = "Day1\\input.txt";
+        int[] input = helper.ReadFileIntoIntArray(fileName);
+
+        Console.WriteLine(input.Length);
+        int lastNumber = 999999999;
+        int counter = 0;
+        for (int i = 0; i<input.Length-2;i++)
+        {
+            Console.WriteLine(input[i]+" "+ input[i+1] +" "+ input[i+2]);
+            //if (number > lastNumber)
+            //{
+            //    counter++;
+            //}
+            //lastNumber = number;
+        }
+        Console.WriteLine("Day1. Challenge 2: " + counter);
+    }
+}
+
+
---
+++ Advent of Code 2021/Program.cs
@@ -0,0 +1,45 @@
+﻿/******************************
+ * Advent of Code  Year 2021  *
+ ******************************/
+
+Day1.Challenge1();
+Day1.Challenge2();
+
+class Helper
+{
+    public string[] ReadFileIntoStringArray(string fileName)
+    {
+        string[] result = File.ReadAllLines("..\\..\\..\\" + fileName);
+        return result;
+    }
+
+    public List<int> ReadFileIntoIntList(string fileName)
+    {
+        List<int> result = new();
+        string[] fileInput = File.ReadAllLines("..\\..\\..\\" + fileName);
+        int number = 0;
+        foreach (string line in fileInput)
+        {
+            if (int.TryParse(line, out number))
+            {
+                result.Add(number);
+            }
+        }
+        return result;
+    }
+
+    public int[] ReadFileIntoIntArray(string fileName)
+    {
+        List<int> result = new();
+        string[] fileInput = File.ReadAllLines("..\\..\\..\\" + fileName);
+        int number = 0;
+        foreach (string line in fileInput)
+        {
+            if (int.TryParse(line, out number))
+            {
+                result.Add(number);
+            }
+        }
+        return result.ToArray();
+    }
+}
